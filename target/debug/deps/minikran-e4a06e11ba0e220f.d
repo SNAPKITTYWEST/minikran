1d89478918a7358f8f8d7f6f8e130dc38e5504d8
SNAPKITTYWEST <jessicalw34@gmail.com>
2026-09-23 10:11:53 -0600

    fix: remove target/ build artifacts, add .gitignore

--- target/debug/deps/minikran-e4a06e11ba0e220f.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jessi\GolandProjects\minikran\target\debug\deps\minikran-e4a06e11ba0e220f.d: src\lib.rs
-
-C:\Users\jessi\GolandProjects\minikran\target\debug\deps\minikran-e4a06e11ba0e220f.exe: src\lib.rs
-
-src\lib.rs:
